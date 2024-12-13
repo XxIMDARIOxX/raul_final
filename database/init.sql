b1540b3ccb993c1a5e7d53bcaf94f39280e7d1e5
Dario <148070311+XxIMDARIOxX@users.noreply.github.com>
2024-12-13 20:16:49 +0100

    fix database

--- database/init.sql
+++ database/init.sql
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS contactos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    mensaje TEXT NOT NULL,
+    mensaje TEXT NOT NULL
 );
 
 --Insert de prueba en contactos
